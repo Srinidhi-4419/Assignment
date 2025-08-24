3ae188b218786f4d098b4fac3a774035f709f49a
Srinidhi-4419 <kulkarnisrinidhi85@gmail.com>
2025-08-24 13:07:06 +0530

    clean-up

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
 
 // Routes
 app.use('/api/forms', formRoutes);
